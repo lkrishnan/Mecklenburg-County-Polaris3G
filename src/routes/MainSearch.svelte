6c5f72a3d010c2c26d8b10ac1f8e904a9fe24228
lakshmanan Krishnan <poo@minnal.attlocal.net>
2023-01-24 17:49:23 -0500

    main search and map

---
+++ src/routes/MainSearch.svelte
@@ -0,0 +1,49 @@
+<div class="md:w-[430px] w-screen p-2">
+    <div class="bg-white border border-gray-300 shadow-lg rounded divide-y divide-gray-300">
+        <div class="flex gap-1 p-1">
+            <button class="inline-flex items-center justify-center w-10 h-10 text-gray-700 transition-colors duration-150 bg-white rounded-full focus:shadow-outline hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
+            
+            <input type="text" 
+                class="flex-grow appearance-none bg-transparent focus:outline-none text-gray-900"
+                aria-label="search for an address or a place">
+            
+            <button class="inline-flex items-center justify-center w-10 h-10 text-gray-700 transition-colors duration-150 bg-white rounded-full focus:shadow-outline hover:bg-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </button>
+
+            <button class="inline-flex items-center justify-center w-10 h-10 text-gray-700 transition-colors duration-150 bg-white rounded-full focus:shadow-outline hover:bg-gray-100 md:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
+                </svg>
+            </button>
+              
+                              
+        </div>
+        <div class="hidden md:flex flex-row p-2 justify-center">
+            <button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2">
+                Adv Search
+            </button>
+
+            <button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2">
+                Market Analysis
+            </button>
+
+            <button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2">
+                Identify
+            </button>
+
+        </div>
+    </div>
+        
+    <!--<div>
+        <input class="appearance-none bg-transparent border-b-4 border-orange-400 dark:border-orange-600 focus:border-orange-500 w-full text-xl md:text-3xl text-gray-900 dark:text-gray-200 mr-3 py-1 px-2 leading-tight focus:outline-none transition-colors duration-200 ease-in-out"
+        type="text"
+        aria-label="search for an address or a place">
+    </div>-->
+</div>
